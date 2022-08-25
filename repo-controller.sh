0d60229cc2db5e1668bc8c93af57cf86b737b329
thangesh babu <thangesh.babu_a@daimler.com>
2022-08-25 22:50:33 +0530

    Initial Commit

---
+++ repo-controller.sh
@@ -0,0 +1,36 @@
+#!/usr/bin/env bash
+
+
+. .env
+
+function usage(){
+cat <<-EOF
+repo-controller [options]
+
+    -h| --help           this is some help text.
+    -1|--first-option    this is my first option
+    -2|--second-option   this is my second option
+EOF
+}
+
+
+function exec_dir(){
+	
+	find /home/bane/work/git/src -maxdepth 1 -name "Sts.*" -type d -exec /bin/bash -c \
+	"pushd {} >/dev/null 2>&1 && echo -e \"[{} :]\n\" &&  $@ 2>&1 ; popd > /dev/null 2>&1 ; echo -e '\n';" \; &
+	wait
+
+}
+
+
+function main(){
+
+	# [[  -z "$@" ]] || usage; return 1 
+	exec_dir "$@"
+
+}
+
+main "$@"
+
+
+
